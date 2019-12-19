aebeb4f7ae8acd10346715c7be5a7b9c2b02c129
Robert <Robert.Elias@live.com>
2019-12-19 13:32:36 -0800

    Updated Form.js FormikUserForm

--- user-onboarding/src/Form.js
+++ user-onboarding/src/Form.js
@@ -1,14 +1,39 @@
-import React {useEffect, useState} from 'react'
-import {Field, Form, withFormik} from 'formik'
-import axios from 'axios'
 import * as Yup from 'yup'
+
+import {Field, Form, withFormik} from 'formik'
+import React, {useEffect, useState} from 'react'
+
+import axios from 'axios'
 
 const UserForm = () => {
     return (
-        <div className="App">
-        <UserForm/>           
+        <div className="user-form">
+        <Form>
+        <Field type="text" name="name" placeholder="What is your name?"/><br></br>
+        <Field type="email" name="email" placeholder="What is your email?"/><br></br>
+        <Field type="password" name="password" placeholder="What is your password?"/><br></br>
+
+        <label className="checkbox-container">Terms of Service
+        <Field type="checkbox" name="terms"/>
+        <span className="checkmark"/>
+        
+        </label>
+
+        </Form>
         </div>
     )
 }
+const FormikUserForm = withFormik({
+    mapPropsToValues({name, email, password, terms}){
+        return{
+            name: name || "",
+            email: email || "",
+            password: password || "",
+            terms: terms || false,
+        };
+
+    }
+    
+})(UserForm);
 
 export default UserForm
